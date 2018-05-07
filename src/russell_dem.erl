fbbd5014870ab2afcee3be0da32d485586103a87
bhuztez <bhuztez@gmail.com>
2018-05-07 18:39:04 +0800

    trivial proofs

--- src/russell_dem.erl
+++ src/russell_dem.erl
@@ -2,14 +2,19 @@
 
 -export([run/1, format_error/1]).
 
-run([Filename, Output]) ->
+run([Filename|Args]) ->
     case parse(Filename) of
         {ok, Forms} ->
             case russell:file_error(Filename, verify_forms(Forms)) of
                 {error, _} = Error ->
                     Error;
                 {ok, Forms1} ->
-                    ok = file:write_file(Output, russell_prim:format(Forms1))
+                    case Args of
+                        [] ->
+                            io:format("~ts~n", [russell_prim:format(Forms1)]);
+                        [Output] ->
+                            ok = file:write_file(Output, russell_prim:format(Forms1))
+                    end
             end;
         {error, _} = Error ->
             Error
@@ -47,7 +52,8 @@ verify_forms(Forms) ->
     State=
         #{vars => #{},
           defs => Defs,
-          df => []},
+          df => [],
+          alias => #{}},
 
     case verify_forms(Forms, State) of
         {ok, Forms1} ->
@@ -89,54 +95,33 @@ verify_prim_forms([H|T], Defs) ->
 
 verify_form({var, _, {symbol, _, Name}, Token}, State = #{vars := Vars}) ->
     {ok, [], State#{vars := Vars#{Name => Token}}};
+verify_form({alias, _, {symbol, _, Name}, Names}, State = #{alias := Alias}) ->
+    {ok, [], State#{alias := Alias#{Name => [N || {name, _, {symbol, _, N}} <- Names]}}};
 verify_form({pp, _, {name, _, {symbol, Line, Name}}, Ins, Out = {def, _, _, _}}, State = #{vars := Vars}) ->
-    Ins1 = subst_vars(Ins, Vars),
-    [X, ':', _] = Out1 = subst_vars(Out, Vars),
-
-    Ins2 =
-        [[{var, V}, ':', maps:get(V, Vars)]
-         || V <- vars([Out1|Ins1])] ++ Ins1,
-
+    {Ins1, [X, ':', _]= Out1} = subst(Ins, Out, Vars),
     Name1 = list_to_atom([$:|atom_to_list(Name)]),
+    {ok,
+     [{def, {Name, Line}, Ins1, Out1},
+      def_equal(Name1, Line, X, X, Vars)],
+     State};
+verify_form({pp, _, {name, _, {symbol, Line, Name}}, Ins, Out = {assert, _, _}}, State) ->
+    {ok, prove_equal('|->', Name, Line, Ins, Out, State), State};
+verify_form({df, _, {name, _, {symbol, Line, Name}}, {def, _, _, _}=Def}, State = #{vars := Vars, df := Df}) ->
+    [X, ':', Y] = subst_vars(Def, Vars),
+    {ok, [def_equal(Name, Line, X, Y, Vars)], State#{df := Df ++ [{X, Y}]}};
+verify_form({prop, _, {name, _, {symbol, Line, Name}}, Ins, Out, Rule}, State = #{defs := Defs}) ->
+    [{def, _, _, Out1}=D1,{def, {Name1, _}, Ins1,_}=D2,P1] = prove_equal('|-<', Name, Line, Ins, Out, State),
 
-    Ins3 = equals(vars(X), Vars),
-    Out2 = [suffix(X, "1"), '=', suffix(X, "2")],
+    {ok, N, US} = apply_rules(Rule, Out1, new_unify(Ins1), State),
+
+    {N1, #{proven := Proven}} = unify_to_proof(Ins1, N, US, maps:to_list(Defs)),
 
     {ok,
-     [{def, {Name, Line}, Ins2, Out1},
-      {def, {Name1, Line}, Ins3, Out2}],
-     State};
-verify_form({pp, _, {name, _, {symbol, Line, Name}}, Ins, Out = {assert, _, _}}, State = #{vars := Vars, df := Df, defs := Defs}) ->
-    Ins1 = subst_vars(Ins, Vars),
-    Out1 = subst_vars(Out, Vars),
+     [D2, make_proof({Name1, Line}, length(Ins1), N1, Proven), D1, P1],
+     State}.
 
-    Ins2 =
-        [[{var, V}, ':', maps:get(V, Vars)]
-         || V <- vars([Out1|Ins1])] ++ Ins1,
-
-    Ins3 = expand(Ins2, Df),
-    Out2 = expand(Out1, Df),
-
-    Name1 = list_to_atom([$@|atom_to_list(Name)]),
-    {ok, P} = search_equals([Out1|Ins2], [Out2|Ins3], new_proof(Ins3), maps:to_list(Defs)),
-    {In, P1} = assert_input_equals(Ins2, Ins3, P),
-    {_, P2} = add_stmt(Out1, {{Name, 0}, In}, P1),
-    {N, #{proven := Proven}} = assert_output_equal(Out1, Out2, P2),
-
-    {ok,
-     [{def, {Name, Line}, Ins2, Out1},
-      {def, {Name1, Line}, Ins3, Out2},
-      make_proof({Name1,Line}, length(Ins2), N, Proven)],
-     State};
-verify_form({df, _, {name, _, {symbol, Line, Name}}, {def, _, X, Y}}, State = #{vars := Vars, df := Df}) ->
-    X1 = subst_vars(X, Vars),
-    Y1 = subst_vars(Y, Vars),
-
-    Ins = equals(vars(X1), Vars),
-    Out = [suffix(X1, "1"), '=', suffix(Y1, "2")],
-
-    {ok, [{def, {Name, Line}, Ins, Out}], State#{df := Df ++ [{X1, Y1}]}}.
-
+def_equal(Name, Line, X, Y, Vars) ->
+    {def, {Name, Line}, equals(vars(X), Vars), [suffix(X, "1"), '=', suffix(Y, "2")]}.
 
 equals(Vs, Vars) ->
     lists:append(
@@ -172,6 +157,15 @@ vars({var, X}, Acc) ->
 vars(_, Acc) ->
     Acc.
 
+subst(Ins, Out, Vars) ->
+    Ins1 = subst_vars(Ins, Vars),
+    Out1 = subst_vars(Out, Vars),
+
+    Ins2 =
+        [[{var, V}, ':', maps:get(V, Vars)]
+         || V <- vars([Out1|Ins1])] ++ Ins1,
+    {Ins2, Out1}.
+
 subst_vars({assert, _, P}, Vars) ->
     ['|-', subst_vars(P, Vars)];
 subst_vars({def, _, X, Y}, Vars) ->
@@ -187,6 +181,31 @@ subst_vars(X, Vars) when is_atom(X) ->
         false ->
             X
     end.
+
+
+prove_equal(P, Name, Line, Ins, Out, #{vars := Vars, df := Df, defs := Defs}) ->
+    {Ins1, Out1} = subst(Ins, Out, Vars),
+    Ins2 = expand(Ins1, Df),
+    Out2 = expand(Out1, Df),
+    Name1 = list_to_atom([$@|atom_to_list(Name)]),
+
+    {Name2, Name3, Ins3, P1, Out3} =
+        case P of
+            '|->' ->
+                {Name1, Name, Ins2, '|-<', Out1};
+            '|-<' ->
+                {Name, Name1, Ins1, '|->', Out2}
+        end,
+
+    {ok, PS} = search_equals([Out1|Ins1], [Out2|Ins2], new_proof(Ins3), maps:to_list(Defs)),
+    {In, PS1} = assert_equal(P1, Ins1, Ins2, PS),
+    {_, PS2} = add_stmt(Out3, {{Name2, 0}, In}, PS1),
+    {[N], #{proven := Proven}} = assert_equal(P, [Out1], [Out2], PS2),
+
+    [{def, {Name1, Line}, Ins1, Out1},
+     {def, {Name, Line}, Ins2, Out2},
+     make_proof({Name3,Line}, length(Ins1), N, Proven)].
+
 
 expand(L, Df) when is_list(L) ->
     expand_df([expand(E, Df) || E <- L], Df);
@@ -224,29 +243,101 @@ search_equals([['|-', H1]|T1], [['|-', H2]|T2], State, Defs) ->
             {error, not_found}
     end.
 
-
-assert_input_equals([], [], State) ->
+assert_equal(_, [], [], State) ->
     {[], State};
-assert_input_equals([H|T1], [H|T2], State = #{known := Known}) ->
-    H1 = maps:get(H, Known),
-    {T3, State1} = assert_input_equals(T1, T2, State),
-    {[H1|T3], State1};
-assert_input_equals([['|-', H1]|T1], [['|-', H2]|T2], State = #{known := Known}) ->
-    A = maps:get(['|-', H2], Known),
+assert_equal(P, [H|T1], [H|T2], State = #{known := Known}) ->
+    {T3, State1} = assert_equal(P, T1, T2, State),
+    {[maps:get(H, Known)|T3], State1};
+assert_equal(P, [['|-', H1]|T1], [['|-', H2]|T2], State = #{known := Known}) ->
+    {X, Y} =
+        case P of
+            '|-<' ->
+                {['|-', H2], ['|-', H1]};
+            '|->' ->
+                {['|-', H1], ['|-', H2]}
+        end,
+    A = maps:get(X, Known),
     E = maps:get([H1, '=', H2], Known),
-    {N, State1} = add_stmt(['|-', H1], {{'|-<', 0}, [A, E]}, State),
-    {T3, State2} = assert_input_equals(T1, T2, State1),
+    {N, State1} = add_stmt(Y, {{P, 0}, [A, E]}, State),
+    {T3, State2} = assert_equal(P, T1, T2, State1),
     {[N|T3], State2}.
 
-assert_output_equal(X, X, State = #{known := Known}) ->
-    {maps:get(X, Known), State};
-assert_output_equal(['|-',X1], ['|-',X2], State = #{known := Known}) ->
-    A = maps:get(['|-', X1], Known),
-    E = maps:get([X1, '=', X2], Known),
-    add_stmt(['|-', X1], {{'|->', 0}, [A, E]}, State).
 
 add_stmt(Stmt, Step, State = #{next_stmt := Next, known := Known, proven := Proven}) ->
     {Next, State#{next_stmt := Next+1, known := Known#{Stmt => Next}, proven := Proven#{Next => Step}}}.
+
+
+new_unify(Ins) ->
+    {_, Vars, Next} = russell_core:subst(Ins, #{}, 0),
+
+    #{ next_var => Next,
+       vars => Vars,
+       next_stmt => length(Ins)+1,
+       stmts => #{},
+       steps => #{},
+       subst => #{}}.
+
+add_stmt(Stmt, State = #{next_stmt := Next, stmts := Stmts}) ->
+    {Next, State#{next_stmt => Next + 1, stmts => Stmts#{Next => Stmt}}}.
+
+apply_rules(Rule, Out, US, State = #{df := Df}) ->
+    {ok, N, US1 = #{stmts := Stmts, vars := Vars, next_var := Next, subst := Subst}} = apply_rule(Rule, US, State),
+    Out1 = expand(Out, Df),
+    {Out2, Vars, Next}= russell_core:subst(Out1, Vars, Next),
+    Out3 = maps:get(N, Stmts),
+    Subst1 = russell_unify:unify(Out2, Out3, Subst),
+    {ok, N, US1#{subst := Subst1}}.
+
+
+apply_rule({subst, _, {symbol,_,Name}, Subst}, State = #{vars := Vars, next_var := Next}, #{alias := Alias, defs := Defs, vars := VarSubst}) ->
+    [Name1] = maps:get(Name, Alias),
+    {ok, Subst1} = make_subst(Subst, #{}, Vars, VarSubst),
+    {Ins, Out} = maps:get(Name1, Defs),
+    {Ins1, Subst2, Next1} = russell_core:subst(Ins, Subst1, Next),
+    {Out1, _, Next2} = russell_core:subst(Out, Subst2, Next1),
+    {Ins2, State1} = lists:mapfoldl(fun add_stmt/2, State#{next_var := Next2}, Ins1),
+    {Out2, State2 = #{steps := Steps}} = add_stmt(Out1, State1),
+    {ok, Out2, State2#{steps := Steps#{Out2 => {Name1, Ins2}}}}.
+
+
+make_subst([], Subst, Vars, _) ->
+    {ok, maps:merge(Vars, Subst)};
+make_subst([{{symbol, Line, V}, Token}|T], Subst, Vars, VarSubst) ->
+    case russell_core:subst(subst_vars(Token, VarSubst), Vars, 0) of
+        {Token1, Vars, 0} ->
+            case maps:find(V, Subst) of
+                {ok, _} ->
+                    {error, {Line, ?MODULE, {subst_defined, V}}};
+                error ->
+                    make_subst(T, Subst#{V => Token1}, Vars, VarSubst)
+            end;
+        _ ->
+            {error, {Line, ?MODULE, {unbound_var_found, V}}}
+    end.
+
+unify_to_proof(Ins, Out, #{vars := Vars, subst := Subst, next_stmt := Next, stmts := Stmts, steps := Steps}, Defs) ->
+    Subst1 =
+        maps:merge(
+          maps:from_list(
+            [ {V, {var, K}}
+              || {K, {var, V}} <- maps:to_list(Vars) ]),
+          Subst),
+
+    {PS, Map} =
+        lists:foldl(
+          fun(N, {PS, Map}) ->
+                  S = russell_unify:subst(maps:get(N, Stmts), Subst1),
+                  case maps:find(N, Steps) of
+                      error ->
+                          {ok, N1, PS1} = russell_search:search(S, PS, Defs);
+                      {ok, {Name, In}} ->
+                          {N1, PS1} = add_stmt(S, {{Name, 0}, [maps:get(I, Map) || I <- In]}, PS)
+                  end,
+                  {PS1, Map#{N => N1}}
+          end,
+          {new_proof(Ins), #{}},
+          lists:seq(length(Ins) + 1, Next - 1)),
+    {maps:get(Out, Map), PS}.
 
 
 make_proof(Name, NIn, N, Steps) ->
--- src/russell_dem_parser.yrl
+++ src/russell_dem_parser.yrl
@@ -1,5 +1,5 @@
-Nonterminals forms form stmts stmt name def assert tokens token.
-Terminals symbol '(' ')' ':' '@' '.' '|-' '.Pp' '.Df'.
+Nonterminals forms form stmts stmt bracket rule num substs subst names name def assert tokens token.
+Terminals symbol '(' ')' '[' ']' ':' '@' '.' '|-' '.Pp' '.Df'.
 Rootsymbol forms.
 
 token -> symbol:
@@ -17,6 +17,12 @@ tokens -> '$empty':
 name -> '@' symbol:
   {name, line_of('$1'), '$2'}.
 
+names -> names name:
+  '$1' ++ ['$2'].
+
+names -> name:
+  ['$1'].
+
 def -> token ':' token:
   {def, line_of('$2'), '$1', '$3'}.
 
@@ -33,7 +39,37 @@ stmts -> stmts stmt:
   '$1' ++ ['$2'].
 
 stmts -> '$empty':
-   [].
+  [].
+
+num -> '(' symbol ')':
+  {num, line_of('$2'), '$2'}.
+
+subst -> symbol token:
+  {'$1', '$2'}.
+
+substs -> substs subst:
+  '$1' ++ ['$2'].
+
+substs -> subst:
+  ['$1'].
+
+rule -> num:
+  '$1'.
+
+rule -> name:
+  '$1'.
+
+rule -> symbol:
+  '$1'.
+
+rule -> name substs:
+  {subst, line_of('$1'), '$1', '$2'}.
+
+rule -> symbol substs:
+  {subst, line_of('$1'), '$1', '$2'}.
+
+bracket -> '[' rule ']':
+  '$2'.
 
 form -> symbol ':' token:
   {var, line_of('$2'), '$1', '$3'}.
@@ -43,6 +79,12 @@ form -> name '.Df' def :
 
 form -> name '.Pp' stmts '.' stmt:
   {pp, line_of('$2'), '$1', '$3', '$5'}.
+
+form -> symbol names '.':
+  {alias, line_of('$1'), '$1', '$2'}.
+
+form -> name stmts '.' stmt bracket:
+  {prop, line_of('$1'), '$1', '$2', '$4', '$5'}.
 
 forms -> forms form:
   '$1' ++ ['$2'].
